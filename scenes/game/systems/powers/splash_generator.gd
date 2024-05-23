4a3c193e23bfe8a0d6a6f29ceed1dd21aed77729
Santiago Alvarez Ricardo <santiagoalvarezricardo@gmail.com>
2024-05-23 17:09:47 -0500

    add some things

--- scenes/game/systems/powers/splash_generator.gd
+++ scenes/game/systems/powers/splash_generator.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var  splaschScene: PackedScene
+@export var  power_scene: PackedScene
 @export var active: bool = false
 @export var group_name: String = "splash"
 
@@ -10,12 +10,12 @@ func _ready():
 
 func  _input(event):
 	if event is InputEventMouseButton and active:
-		var new_splash: Splash = splaschScene.instantiate()
-		new_splash.position = event.position
-		new_splash.add_to_group(group_name)
-		get_parent().add_child(new_splash)
+		var new_power: Splash = power_scene.instantiate()
+		new_power.position = event.position
+		new_power.add_to_group(group_name)
+		get_parent().add_child(new_power)
 	
-
+func set_power(power: int):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
